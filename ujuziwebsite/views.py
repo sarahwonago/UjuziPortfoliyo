50a0c042e85ae0267ab40923a22ec9fc2707dbac
sarahwonago <sarahshaaleh@gmail.com>
2024-06-15 15:40:23 +0300

    modifications

--- ujuziwebsite/views.py
+++ ujuziwebsite/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home_view(request):
+    return render(request, "ujuziwebsite/index.html")
+
+def about_view(request):
+    return render(request, "ujuziwebsite/about.html")
+
+def contact_view(request):
+    return render(request, "ujuziwebsite/contact.html")
