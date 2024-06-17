2edcdd856650317077204f6fc21b71c02ef550b0
sarahwonago <sarahshaaleh@gmail.com>
2024-06-17 15:02:18 +0300

    changes based on user authentication

--- ujuziportfoliyo/urls.py
+++ ujuziportfoliyo/urls.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     #Admin
     path("admin/", admin.site.urls),
 
-    #Auth app
-    path("accounts/", include('django.contrib.auth.urls')),
-
     #Custom
+    path("accounts/", include('accounts.urls')),
     path("", include('ujuziwebsite.urls')),
     path("dashboard/", include('userdashboard.urls')),
-    path("accounts/", include('accounts.urls')),
-]
+    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
