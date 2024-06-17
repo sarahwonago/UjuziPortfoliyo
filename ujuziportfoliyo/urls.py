64785f6020af28e21add22ae73cd198e5dfe3f03
sarahwonago <sarahshaaleh@gmail.com>
2024-06-17 10:57:07 +0300

    adding auth app urls

--- ujuziportfoliyo/urls.py
+++ ujuziportfoliyo/urls.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    #Admin
     path("admin/", admin.site.urls),
+
+    #Auth app
+    path("accounts/", include('django.contrib.auth.urls')),
+
+    #Custom
     path("", include('ujuziwebsite.urls')),
+    path("dashboard/", include('userdashboard.urls')),
+    path("accounts/", include('accounts.urls')),
 ]
