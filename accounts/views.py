eab6b2745ce2011d45ff901e9b9896c079ac6527
sarahwonago <sarahshaaleh@gmail.com>
2024-06-17 15:00:54 +0300

    views for handling user authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 def logout_view(request):
-    pass
+    logout(request)
+    return redirect("ujuziwebsite:home")
+
+def login_view(request):
+
+    if request.user.is_authenticated:
+        return redirect("ujuziwebsite:home")
+    
+    
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("ujuziwebsite:home")
+    
+    return render(request, "accounts/registration/login.html")
+
+def register_view(request):
+
+    if request.user.is_authenticated:
+        return redirect("ujuziwebsite:home")
+       
+    
+    return render(request, "accounts/registration/user_registration.html")
