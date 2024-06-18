a364d22b2e8d6cc920a1a1ee84895bfc01d60b06
sarahwonago <sarahshaaleh@gmail.com>
2024-06-18 18:05:42 +0300

    adding userportfoliyo to main projects

--- ujuziportfoliyo/urls.py
+++ ujuziportfoliyo/urls.py
@@ -8,8 +8,9 @@ urlpatterns = [
     path("admin/", admin.site.urls),
 
     #Custom
+    path("userportfolio/", include('userportfoliyo.urls')),
+    path("dashboard/", include('userdashboard.urls')),
     path("accounts/", include('accounts.urls')),
     path("", include('ujuziwebsite.urls')),
-    path("dashboard/", include('userdashboard.urls')),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
